cde2f35a8814c4a066842e72b4b669e1d1ecda21
vanmuoi24 <domuoigghh@gmail.com>
2025-09-18 20:45:37 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "public", // mặc định là 'public', nhưng chắc chắn để đúng
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public", // đảm bảo copy cả _redirects
 });
