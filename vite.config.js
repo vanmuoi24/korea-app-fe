0ce05740b7f2c7f9bc1175e7f1964e021e3b3425
vanmuoi24 <domuoigghh@gmail.com>
2025-09-18 20:51:00 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "public/_redirects", dest: "" }],
+    }),
+  ],
   build: {
     outDir: "dist",
   },
-  publicDir: "public", // đảm bảo copy cả _redirects
 });
